d2a4f1e2644f39f96b7f10ddd7f5402c99cddb46
ke_lu <410231238@qq.com>
2017-04-19 13:49:29 +0800

    aop

--- src/main/java/com/kat/util/testAop.java
+++ src/main/java/com/kat/util/testAop.java
@@ -5,18 +5,22 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
-public class aop2 {
+@Aspect
+public class testAop {
 
-    public aop2() {
-        System.out.println("aop");
+    public testAop() {
+        System.out.println("testAop");
 
     }
 
+    //拦截所有公共方法
+    @Pointcut("execution(public * *(..))")
     public void aoppoint() {
 
     }
 
-    public void bef() {
+    @Before("aoppoint()")
+    public void before() {
         System.out.println("before");
     }
 }
